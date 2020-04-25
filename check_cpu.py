ef7e18fb5a805d98082b715724de59bafb66c4fb
Amitabh Baruah <abaruah@localhost.localdomain>
2020-04-25 15:58:09 +0530

    new features added

--- check_cpu.py
+++ check_cpu.py
@@ -1,6 +1,26 @@
 import os
 import psutil
-pid = os.getpid()
-py = psutil.Process(pid)
-memoryUse = py.memory_info()[0]/2.**30 
-print('memory currently in use  :', memoryUse)
+import subprocess
+
+threshold=40
+partition="/"
+
+def cpu_utilization():
+    pid = os.getpid()
+    py = psutil.Process(pid)
+    memoryUse = py.memory_info()[0]/2.**30
+    print('memory currently in use  :', memoryUse)
+
+
+def check_memory_usage():
+     df = subprocess.Popen(['df','-h'], stdout=subprocess.PIPE)
+     for line in df.stdout:
+        splitline = line.decode().split()
+        if splitline[5] == partition:
+            if int(splitline[4][:-1]) > threshold:
+                print('Low disk space warning')
+
+cpu_utilization()
+check_memory_usage()
+
+
