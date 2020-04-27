a60e3ca9a33de4341704cb01ce54136ce95045f6
Your Name <amitabhyo@gmail.com>
2020-04-28 01:30:05 +0530

    Some more messages added

--- check_cpu.py
+++ check_cpu.py
@@ -1,30 +1,38 @@
 import os
+import getpass
 import psutil
 import subprocess
 import socket
 import platform
+import datetime
+
 
 threshold_value=40
 partition="/"
+
+
+
 
 def check_no_network():
     """Returns true if it fails to resolve Google;s url , false otherwise"""
     try:
         socket.gethostbyname("www.google.com")
-        print('Network working')
+        print('CONNECTED TO NETWORK...')
         return False 
     except:
         return True
 
 def os_specs():
-    print(platform.system(),'  ',platform.release())
+    print('Hello',getpass.getuser())
+    print('Time :',datetime.datetime.now())
+    print('OS : ',platform.system(),'',platform.release())
 
 
 def cpu_utilization():
     pid = os.getpid()
     py = psutil.Process(pid)
     memoryUse = py.memory_info()[0]/2.**30
-    print('memory currently in use  :', memoryUse)
+    print('Memory currently in use  :', memoryUse)
 
 
 def check_memory_usage():
@@ -33,10 +41,14 @@ def check_memory_usage():
         splitline = line.decode().split()
         if splitline[5] == partition:
             if int(splitline[4][:-1]) > threshold_value:
-                print('Low disk space alert')
+                print('Low disk space alert !')
 
+
+
+print('--------------------------------------------')
 os_specs()
 cpu_utilization()
 check_memory_usage()
 check_no_network()
 
+print('--------------------------------------------')
